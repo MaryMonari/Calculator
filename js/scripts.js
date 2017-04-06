94f0045dbb60b93deac534497c725434f7d18c1d
MaryMonari <moraamarymonari@gmail.com>
2017-04-06 17:45:52 +0300

    added adding function

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Adding Function
+var add= function(number1, number2){
+  return number1 + number2;
+}
+
+// Collecting input from user
+var number1 =parseInt(prompt("Enter a number:"));
+var number2 =parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
